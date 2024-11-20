553eb55bde21a07ee468f7a5ed6ea5788cc6d88a
Arymus <arymusr@gmail.com>
2024-11-20 15:55:13 -0500

    Add files via upload

---
+++ CWD Project/script.js
@@ -0,0 +1,36 @@
+const input = document.getElementById("input");
+const enter = document.getElementById("enter");
+const enterLink = document.getElementById("enterLink");
+const hint = document.getElementById("hint");
+const password = document.getElementById("password");
+
+function enterPassword() {
+    if (input.value === "perrito" || input.value === "perrita") {
+        enterLink.href ="main.html";
+    } else if (input.value === "") {
+        alert("Type in a password!")
+    } else if (input.value === "password") {
+        alert("Nice try.");
+    } else {
+        alert("Incorrect password! Try again :(");
+    };
+}
+
+function getHint() {
+    const hintArray = ["The spanish word for \"puppy\"!", "A young dog (in spanish)", "Someone who likes chew toys (in spanish)"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    alert(hintArray[randomNumber]);
+}
+
+enter.addEventListener("click", enterPassword);
+enter.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        enterPassword();
+    }
+});
+
+hint.addEventListener("click", getHint);
+
+password.addEventListener("click", function() {
+    alert("Psst.. The password is \"perrito\" or \"perrita\"!");
+})
